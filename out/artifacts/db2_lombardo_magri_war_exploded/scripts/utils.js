954ed2c0421bec8831e5cce8dce524a92a9f62ae
Gianluca Lombardo <10628593@polimi.it>
2021-12-17 16:40:43 +0100

    Commit entities and some fixes

---
+++ out/artifacts/db2_lombardo_magri_war_exploded/scripts/utils.js
@@ -0,0 +1,22 @@
+/**
+ * AJAX
+ */
+
+function makeCall(method, url, formElement, callback, reset = true) {
+    var req = new XMLHttpRequest(); // visible by closure
+    req.onreadystatechange = function() {
+        callback(req)
+    };
+
+    req.open(method, url);
+
+    if (formElement == null) {
+        req.send();
+    } else {
+        req.send(new FormData(formElement));
+    }
+
+    if (formElement !== null && reset === true) {
+        formElement.reset();
+    }
+}
